a9d4ff0e8e397d1e1c0837eea942157e9badf4bf
Michal Boese <boesem93@gmail.com>
2023-12-19 23:25:40 +0100

    Dzień pierwszy gotowy

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,26 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
 int main() {
-    string example = "1abc33dsa";
-    string values;
+    system("cd");
+    vector<string> input;
+    ifstream inputFile("input.txt");
+
+    if (inputFile.fail()) {
+        cerr << strerror(errno) << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(inputFile, line)) {
+        input.push_back(line);
+    }
+
+    vector<string> lineValues;
+
     int sum = 0;
+    for (string s : input) {
+        string currentLine;
+        size_t firstDigitPos = s.find_first_of("0123456789");
+        if (firstDigitPos != string::npos) {
+            currentLine += s[firstDigitPos];
+        }
 
-    size_t firstDigitPos = example.find_first_of("0123456789");
-    if (firstDigitPos != string::npos) {
-        values += example[firstDigitPos];
+        size_t lastDigitPos = s.find_last_of("0123456789");
+        if (lastDigitPos != string::npos) {
+            currentLine += s[lastDigitPos];
+        }
+        lineValues.push_back(currentLine);
     }
 
-    size_t lastDigitPos = example.find_last_of("0123456789");
-    if (lastDigitPos != string::npos) {
-        values += example[firstDigitPos];
+    for (const string& s : lineValues) {
+        sum += stoi(s);
     }
 
-
-
-    cout << "Sum: " << values << endl;
+    cout << sum << endl;
 
     return 0;
 }
